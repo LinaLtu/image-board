ff0229ec6a0f0a1435dbb736305cd76d6b097556
Lina Suodyte <lsuodyte@gmail.com>
2018-04-10 22:22:17 +0200

    Added README

---
+++ README.md
@@ -0,0 +1,30 @@
+
+
+### Image Board – CactusImages
+
+On the 8th week of Full Stack JavaScript Web Development coding bootcamp at SPICED, I build a single page, responsive image board, where users can upload, view and comment on images.
+
+![imageboardscreenshot](/Users/lina/Desktop/horseradish-imageboard/imageboardscreenshot.jpg)
+
+### Users can:
+
+- Upload images with a description,
+
+- Open a modal with a bigger image, where they can leave a comment
+
+  ![imageboarddemo](/Users/lina/Desktop/horseradish-imageboard/imageboarddemo.gif)
+
+### Technologies used:
+
+- Vue.js
+
+- Node.js and Express.js
+
+- PostgreSQL Database
+
+- AWS S3 Image Storage
+
+  ​
+
+  This project was my first application built using  Vue.js. I also had an opportunity to get familiar with AWS S3.
+
--- public/js/script.js
+++ public/js/script.js
@@ -1,15 +1,15 @@
-Vue.component("single-image", {
-    props: ["id"],
+Vue.component('single-image', {
+    props: ['id'],
     data: function() {
         return {
             image: {
-                title: "",
-                description: "",
-                username: "",
+                title: '',
+                description: '',
+                username: '',
                 id: null
             },
             error: false,
-            comment: "",
+            comment: '',
             comments: [],
             commentInfo: {},
             selectedImage: null
@@ -17,7 +17,7 @@ Vue.component("single-image", {
     },
     methods: {
         exit: function() {
-            this.$emit("done");
+            this.$emit('done');
         },
         handleCommentSubmit: function(e) {
             // e.preventDefault();
@@ -25,24 +25,24 @@ Vue.component("single-image", {
             this.commentInfo.username;
             this.commentInfo.description;
             console.log(
-                "Comment Info: ",
+                'Comment Info: ',
                 this.commentInfo.username,
                 this.commentInfo.description
             );
-            console.log("From axios ", this.id);
+            console.log('From axios ', this.id);
             axios
-                .post("/comments", {
+                .post('/comments', {
                     comments: this.commentInfo.description,
                     username: this.commentInfo.username,
                     image_id: this.id
                 })
                 .then(function(resp) {
-                    console.log("From axios ", this.commentInfo.description);
+                    console.log('From axios ', this.commentInfo.description);
                     self.comments.unshift(resp.data.results);
                 });
         }
     },
-    template: "#big-image",
+    template: '#big-image',
     mounted: function() {
         // console.log("Mounted ran");
         if (isNaN(this.id)) {
@@ -52,18 +52,18 @@ Vue.component("single-image", {
         console.log(this);
         var self = this;
         // console.log(self.id);
-        axios.get("/images/" + this.id).then(function(resp) {
+        axios.get('/images/' + this.id).then(function(resp) {
             if (!resp.data.image) {
                 self.error = true;
             }
             if (!resp.data.success) {
-                self.$emit("done");
+                self.$emit('done');
             } else {
                 self.image = resp.data.image;
                 console.log(self);
             }
 
-            axios.get("/comments/" + self.id).then(function(resp) {
+            axios.get('/comments/' + self.id).then(function(resp) {
                 console.log(resp);
                 self.comments = resp.data.comments;
             });
@@ -76,13 +76,13 @@ Vue.component("single-image", {
                 return;
             }
             var self = this;
-            axios.get("/images/" + this.id).then(function(resp) {
+            axios.get('/images/' + this.id).then(function(resp) {
                 if (!resp.data.success) {
-                    self.$emit("done");
+                    self.$emit('done');
                 } else {
                     self.image = resp.data.image;
                     console.log(self);
-                    axios.get("/comments/" + self.id).then(function(resp) {
+                    axios.get('/comments/' + self.id).then(function(resp) {
                         self.comments = resp.data.comments;
                     });
                 }
@@ -92,14 +92,14 @@ Vue.component("single-image", {
 });
 
 new Vue({
-    el: "#main", //where our app will load
+    el: '#main', //where our app will load
     data: {
         images: [],
         selectedImage: location.hash.slice(1) || null,
         formInfo: {
-            title: "",
-            description: "",
-            username: "",
+            title: '',
+            description: '',
+            username: '',
             file: void 0
         }
     },
@@ -108,82 +108,82 @@ new Vue({
             this.formInfo.file = e.target.files[0]; //???
         },
 
-        handleScroll: function(e) {
-            // $(window).scroll(function() {
-            //     if (
-            //         $(window).scrollTop() + $(window).height() ==
-            //         $(document).height()
-            //     ) {
-            //         alert("bottom!");
-            //     }
-            // });
-
-            //if it's not scrolled to the bottom, then {return}
-
-            console.log("Running scroll"); //running as soon as the page uploads
-            //scrollTop -> run handleScroll
-            //get the correct number of images being displayed (this.images.length = offset)
-            //then use the offset number
-            var body = document.body,
-                html = document.documentElement;
-
-            var documentHeight = Math.max(
-                body.scrollHeight,
-                body.offsetHeight,
-                html.clientHeight,
-                html.scrollHeight,
-                html.offsetHeight
-            );
-
-            var scrollBottom = html.clientHeight + window.scrollY;
-
-            console.log(scrollBottom);
-
-            //This is where we have problems - we should not make another request if the current one is still running
-            if (documentHeight == scrollBottom) {
-                alert("Bottom!!");
-
-                console.log(this.images.length);
-                // var offset = window.offset + 6;
-                var app = this;
-
-                //This event listener gets removed and we do not check if scroll occurs again
-                window.removeEventListener("scroll", this.handleScroll);
-
-                axios.get("/imagesList/" + offset).then(function(results) {
-                    window.requestOpen = true;
-
-                    var newData = app.images.concat(results.data.images);
-                    app.images = newData;
-                    var resultsLength = results.data.images.length;
-
-                    console.log("Total results: ", results);
-                });
-            }
-        },
+        // handleScroll: function(e) {
+        //     // $(window).scroll(function() {
+        //     //     if (
+        //     //         $(window).scrollTop() + $(window).height() ==
+        //     //         $(document).height()
+        //     //     ) {
+        //     //         alert("bottom!");
+        //     //     }
+        //     // });
+        //
+        //     //if it's not scrolled to the bottom, then {return}
+        //
+        //     console.log("Running scroll"); //running as soon as the page uploads
+        //     //scrollTop -> run handleScroll
+        //     //get the correct number of images being displayed (this.images.length = offset)
+        //     //then use the offset number
+        //     var body = document.body,
+        //         html = document.documentElement;
+        //
+        //     var documentHeight = Math.max(
+        //         body.scrollHeight,
+        //         body.offsetHeight,
+        //         html.clientHeight,
+        //         html.scrollHeight,
+        //         html.offsetHeight
+        //     );
+        //
+        //     var scrollBottom = html.clientHeight + window.scrollY;
+        //
+        //     console.log(scrollBottom);
+        //
+        //     //This is where we have problems - we should not make another request if the current one is still running
+        //     if (documentHeight == scrollBottom) {
+        //         alert("Bottom!!");
+        //
+        //         console.log(this.images.length);
+        //         // var offset = window.offset + 6;
+        //         var app = this;
+        //
+        //         //This event listener gets removed and we do not check if scroll occurs again
+        //         window.removeEventListener("scroll", this.handleScroll);
+        //
+        //         axios.get("/imagesList/" + offset).then(function(results) {
+        //             window.requestOpen = true;
+        //
+        //             var newData = app.images.concat(results.data.images);
+        //             app.images = newData;
+        //             var resultsLength = results.data.images.length;
+        //
+        //             console.log("Total results: ", results);
+        //         });
+        //     }
+        // },
         handleSubmit: function(e) {
             e.preventDefault();
             const formData = new FormData(); //this is how we send an image
-            formData.append("title", this.formInfo.title);
-            formData.append("description", this.formInfo.description);
-            formData.append("username", this.formInfo.username);
-            formData.append("file", this.formInfo.file);
+            formData.append('title', this.formInfo.title);
+            formData.append('description', this.formInfo.description);
+            formData.append('username', this.formInfo.username);
+            formData.append('file', this.formInfo.file);
 
-            axios.post("/upload", formData).then(results => {
+            axios.post('/upload', formData).then(results => {
                 // console.log("Results from Axios", results);
-                this.formInfo.title = "";
-                this.formInfo.description = "";
-                this.formInfo.username = "";
-                this.formInfo.file = "";
+                this.formInfo.title = '';
+                this.formInfo.description = '';
+                this.formInfo.username = '';
+                this.formInfo.file = '';
 
                 this.images.unshift(results.data);
 
-                console.log("Images:", this.images);
+                console.log('Images:', this.images);
             });
         },
         hideImage: function() {
             this.selectedImage = null;
-            location.hash = "";
+            location.hash = '';
         }
     },
     mounted: function() {
@@ -191,25 +191,25 @@ new Vue({
         var app = this;
         window.offset = 0;
 
-        axios.get("/imagesList/0").then(function(results) {
+        axios.get('/imagesList/0').then(function(results) {
             app.images = results.data.images;
         });
 
-        window.addEventListener("hashchange", function() {
+        window.addEventListener('hashchange', function() {
             // console.log(typeof parseInt(location.hash.slice(1)));
             if (!(parseInt(location.hash.slice(1)) == location.hash.slice(1))) {
-                console.log("We are heeeeeere");
-                location.hash = "";
+                console.log('We are heeeeeere');
+                location.hash = '';
                 app.selectedImage = null;
             } else {
                 app.selectedImage = location.hash.slice(1);
             }
         });
-    },
-    created: function() {
-        window.addEventListener("scroll", this.handleScroll);
-    },
-    destroyed: function() {
-        window.removeEventListener("scroll", this.handleScroll);
     }
+    // created: function() {
+    //     window.addEventListener('scroll', this.handleScroll);
+    // },
+    // destroyed: function() {
+    //     window.removeEventListener('scroll', this.handleScroll);
+    // }
 });
